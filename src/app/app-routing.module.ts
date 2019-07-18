4e13938d5460849e900073141c68bfc8163c9bab
GelenMR <gelen.montilla.r@gmail.com>
2019-07-18 17:22:34 -0400

    Se agregar productos del menu y otros elementos a los diferentes componenetes: Breakfast, Lunch, Aggregates, Order, Header, Main, Waiter, Kitcken

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { WaiterComponent } from './components/waiter/waiter.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { KitchenComponent } from './components/kitchen/kitchen.component';
+import { BreakfastComponent } from './components/breakfast/breakfast.component';
+import { LunchComponent } from './components/lunch/lunch.component';
 
 
 const routes: Routes = [
   { path: 'waiter', component: WaiterComponent },
   { path: 'kitchen', component: KitchenComponent },
+  { path: 'breakfast', component: BreakfastComponent },
+  { path: 'lunch', component: LunchComponent },
   { path: '', component: WelcomeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {enableTracing:false}
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
