13ec7ca04106d2e8f89b3e7687d8065f9842b7a2
GelenMR <gelen.montilla.r@gmail.com>
2019-07-18 15:39:25 -0400

    pp. Inicializacion de rutas, se crea app-routing.module, se construye array de rutas, se adhieren elementos dom a componente welcome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { KitchenComponent } from './components/kitchen/kitchen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'waiter', component: WaiterComponent },
+  { path: 'kitchen', component: KitchenComponent },
+  { path: '', component: WelcomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
