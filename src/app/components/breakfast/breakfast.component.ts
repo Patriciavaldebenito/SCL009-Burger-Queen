e881b23c65579062c6ccaa20cc079270b5cf38b9
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-09-05 13:09:13 -0400

    orden codigo y variables en breakfast.component.ts

--- src/app/components/breakfast/breakfast.component.ts
+++ src/app/components/breakfast/breakfast.component.ts
@@ -16,44 +16,21 @@ export class BreakfastComponent implements OnInit {
 
    arr = [];
 
-//  ************************************************************************
   //b.- inyeccion de dependencia - servicio 
   constructor(public informationOrderService: InformationOrderService) { 
     this.informationOrderService = informationOrderService;
   }
  
- //  ************************************************************************
+
   //c.- subscribir al subject que expone el servicio inyectado
-  ngOnInit() { this.informationOrderService.sendProductObservable.subscribe( arr=> this.arr = arr);
-                                                                            }
-  //                                                                        arr = order en el servicio
-
+  ngOnInit() { this.informationOrderService.sendProductObservable.subscribe( arr=> this.arr = arr);  }
+  
   /* ***   Evento   Click   ***/
-
-
-
-
-
   onSelect(selectedClassMenu): void {
-    
-    // inicio/seguimiento   -     el array es vacio 
-    console.log(" brakfast  -- arr antes de ser llenado :" + this.arr)
+    console.log("arr :" + this.arr)
     console.log(this.arr)
-    // pero ahora se guardara el elemento / producto clickeado
     this.arr.push(selectedClassMenu)
-    // listo el producto esta en el array 
-    console.log(" objeto por producto :" + JSON.stringify(selectedClassMenu))
-    console.log(" brakfast  -push- dps de ser llenado :" + JSON.stringify(this.arr))
-    
-    // ahora arr sera parametro de la funcion setOrder del InformationOrderServer importado en el constructor 
-    // e iniciado (ver ngOnInit ) del componente
-    //this.informationOrderService.setOrder(this.arr)
     this.informationOrderService.observeProduct(this.arr)
-
   }
-
-
-
-
 
 }
