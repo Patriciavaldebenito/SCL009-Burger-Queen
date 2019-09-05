91e59482fe4f7b06dcd11ec8abb289c1906fe12a
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-09-05 13:11:02 -0400

    Se elimina servidor sin uso show desde ruta src/app

--- src/app/components/lunch/lunch.component.ts
+++ src/app/components/lunch/lunch.component.ts
@@ -28,7 +28,6 @@ export class LunchComponent implements OnInit {
   onSelect(selectedClassMenu): void {
     this.arr.push(selectedClassMenu)
     this.informationOrderService.observeProduct(this.arr)
-
   }
 
 }
